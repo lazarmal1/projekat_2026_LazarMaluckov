c7682edbe097eb543b0191470f3e5c76529c09b0
ucenik <ucenik@DESKTOP-M7J8SQ9>
2026-03-18 10:28:02 +0100

    V 1.0 Login

--- Login.cs
+++ Login.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace projekat_2026_LazarMaluckov
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Lazar Maluckov 
+            // drugi red
         }
     }
 }
